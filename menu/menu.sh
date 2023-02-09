53a50004ad13610bdd12c6607784e0f53a18baf2
Ross Brunson <r.brunson@f5.com>
2023-02-09 11:32:12 -0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -10,11 +10,9 @@ do
         "Wipe")
            sudo rm -rf /data /home/ubuntu /home/student/Desktop/Double-Click-2-Run-Labs /home/student/NGINX-Fundamentals-Labs/ /home/student/Desktop/Lab_Files /etc/nginx/conf.d/my*.* /var/log/nginx/*{access,error}.log && echo "Wipe Completed Successfuly." ;;
         "Setup")
-           wget -qO - https://raw.githubusercontent.com/learnf5/nginx/main/_overall-setup-script/ngx-fund-ilt-setup.sh | sudo bash
-           clear ;;
+           wget -qO - https://raw.githubusercontent.com/learnf5/nginx/main/_overall-setup-script/ngx-fund-ilt-setup.sh | sudo bash ;;
         "Mopup")
-           sudo mv /etc/nginx/conf.d/default.{bak,conf} ; ls -l /etc/nginx/conf.d/ ; history -c
-           clear ;;
+           sudo mv /etc/nginx/conf.d/default.{bak,conf} ; ls -l /etc/nginx/conf.d/ ; history -c ;;
         "Quit")
            echo "We're done"
            break;;
