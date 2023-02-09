7ddfe55f3f7093f7b0bcce0d2e7a8ae427b077e0
Ross Brunson <r.brunson@f5.com>
2023-02-09 11:20:42 -0700

    Fix to menu clears

--- menu/menu.sh
+++ menu/menu.sh
@@ -6,11 +6,9 @@ select act in Query Wipe Setup Mopup Quit
 do
     case $act in
         "Query")
-            ls -ld /data /home/ubuntu /home/student/Desktop/Double-Click-2-Run-Labs /home/student/NGINX-Fundamentals-Labs/ /home/student/Desktop/Lab_Files /etc/nginx/conf.d/{my,ret,ssl}*.*/var/log/nginx/*{access,error}.log 
-            clear ;;
+            ls -ld /data /home/ubuntu /home/student/Desktop/Double-Click-2-Run-Labs /home/student/NGINX-Fundamentals-Labs/ /home/student/Desktop/Lab_Files /etc/nginx/conf.d/{my,ret,ssl}*.*/var/log/nginx/*{access,error}.log ;;
         "Wipe")
-           sudo rm -rf /data /home/ubuntu /home/student/Desktop/Double-Click-2-Run-Labs /home/student/NGINX-Fundamentals-Labs/ /home/student/Desktop/Lab_Files /etc/nginx/conf.d/my*.* /var/log/nginx/*{access,error}.log && echo "Wipe Completed Successfuly."
-           clear ;;
+           sudo rm -rf /data /home/ubuntu /home/student/Desktop/Double-Click-2-Run-Labs /home/student/NGINX-Fundamentals-Labs/ /home/student/Desktop/Lab_Files /etc/nginx/conf.d/my*.* /var/log/nginx/*{access,error}.log && echo "Wipe Completed Successfuly." ;;
         "Setup")
            wget -qO - https://raw.githubusercontent.com/learnf5/nginx/main/_overall-setup-script/ngx-fund-ilt-setup.sh | sudo bash
            clear ;;
