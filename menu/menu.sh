b56f2c4fbad70eb20a1c4e880d8afa9528cd90ef
Ross Brunson <r.brunson@f5.com>
2023-02-13 10:31:29 -0700

    Updated menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -10,11 +10,12 @@ do
         "Query")
             ls -ld /data /home/ubuntu /home/student/Desktop/Double-Click-2-Run-Labs /home/student/NGINX-Fundamentals-Labs/ /home/student/Desktop/Lab_Files /etc/nginx/conf.d/{my*,re*,ssl*}.* /var/log/nginx/*{access,error}.log /etc/nginx/conf.d/default.* ;;
         "Wipe")
-           sudo rm -rf /data /home/ubuntu /home/student/Desktop/Double-Click-2-Run-Labs /home/student/NGINX-Fundamentals-Labs/ /home/student/Desktop/Lab_Files /etc/nginx/conf.d/{my*,re*,ssl*}.* /var/log/nginx/*{access,error}.log && echo "Wipe Completed Successfuly." ;;
+           sudo rm -rf /data /home/ubuntu /home/student/Desktop/Double-Click-2-Run-Labs /home/student/NGINX-Fundamentals-Labs/ /home/student/Desktop/Lab_Files /etc/nginx/conf.d/*.* /var/log/nginx/*{access,error}.log && echo "Wipe Completed Successfuly." ;;
         "Setup")
            wget -qO - https://raw.githubusercontent.com/learnf5/nginx/main/_overall-setup-script/ngx-fund-ilt-setup.sh | sudo bash ;;
         "Mopup")
-           sudo mv /etc/nginx/conf.d/default.{bak,conf} ; ls -l /etc/nginx/conf.d/ 
+           sudo wget -qP /etc/nginx/conf.d https://raw.githubusercontent.com/learnf5/nginx/main/lab02/default.conf ; ls -l /etc/nginx/conf.d/ 
+           sudo -i history -c
            history -c ;;
         "Edit")
            sudo vim menu.sh ;;
