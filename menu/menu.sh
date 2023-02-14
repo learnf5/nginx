ce3885210deb402fb0c95ce48f6bef4fad540b43
Ross Brunson <r.brunson@f5.com>
2023-02-14 15:25:39 -0700

    Menu 4 update

--- menu/menu.sh
+++ menu/menu.sh
@@ -14,8 +14,7 @@ do
         "Setup")
            wget -qO - https://raw.githubusercontent.com/learnf5/nginx/main/_overall-setup-script/ngx-fund-ilt-setup.sh | sudo bash ;;
         "Mopup")
-           sudo -i history -c
-           history -c ;;
+           sudo -i history -c && history -c && echo "Histories Cleared";;
         "Edit")
            sudo vim menu.sh ;;
         "Quit")
