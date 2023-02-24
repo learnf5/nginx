1b55aea74cc8f615ee495750c5a8b7422205d117
Ross Brunson <r.brunson@f5.com>
2023-02-24 16:24:45 -0700

    Remove extra comments and add needed comments

--- _overall-setup-script/ngx-fund-ilt-setup.sh
+++ _overall-setup-script/ngx-fund-ilt-setup.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 #
 # PURPOSE:  Script to setup a stock NGINX Fundamentals Workstation for ILT Labs
+#####################################################################
+# SYSTEM REQUIREMENTS:
+#   The NGINX Fundamentals should be setup on a system matching:
+#   OS:  Ubuntu 22.04.1 Jammy Jellyfish
+#   RAM: 4 GB
+#   Disk:  25 GB, configured as EXT4 or the default
+#
+######################################################################
+# Needed Packages:
+#    - vim and vim-common
+#    - tree
+#    - NGINX OSS 1.2.1 and NGINX Plus R27 or higher     
 #
 #####################################################################
 ## Methods to Execute this Script on Lab Systems
 #####################################################################
-#
-#####################################################################
+###
 ### To do the setup with confirmations at each Step use the below command:
 ###
 ### sudo -i <enter the password if prompted> 
 ### bash <(curl -s https://raw.githubusercontent.com/learnf5/nginx/main/_overall-setup-script/ngx-fund-ilt-setup.sh)
 #####################################################################
+###
 ### To do the setup with no confirmations, use the below command:
 ###
 ### wget -qO - https://raw.githubusercontent.com/learnf5/nginx/main/_overall-setup-script/ngx-fund-ilt-setup.sh | sudo bash
 #####################################################################
-#
-#####################################################################
-## Use the menu.sh script to have a menu for query/wipe/setup
+###
+### Use the menu.sh script to have a menu for query/wipe/setup
+###
 #####################################################################
 ###
 ### wget -qO - https://raw.githubusercontent.com/learnf5/nginx/main/menu/menu.sh > menu.sh
 ### chmod +x menu.sh ; ./menu.sh
 #####################################################################
-#
-## This is the start of the script
+###
+### END OF INSTRUCTIONS
+###
+#####################################################################
 
 echo "#############################################################################"
 echo " Step 1 of 6  - Starting the Lab System Setup"
 echo "#############################################################################"
 read -p "You will be prompted to press the RETURN key after each phase of the prep executes"
-# sudo apt install -y tree
-
-## Get and place NGINX Fundamentals Labs and make a desktop icon
 
 echo "#############################################################################"
 echo " Step 2 of 6 - Pull down HTML Labs and Make Desktop Icon"
@@ -51,8 +62,6 @@ ls -l /home/student/NGINX-Fundamentals-Labs/index.html && STEP2="OK - NGINX Lab 
 
 read -p "Press the ENTER key to proceed"
 
-## Get and place the public_html contents
-
 echo "#############################################################################"
 echo " Step 3 of 6  - Pull down and expand the public_html tree"
 echo "#############################################################################"
@@ -68,8 +77,6 @@ echo "--------------------------------------------------------------------------
 ls -ld /home/ubuntu/public_html && STEP3="OK - public_html directory Verified"
 
 read -p "Press the ENTER key to proceed"
-
-## Get and place the data/images contents
 
 echo "#############################################################################"
 echo " Step 4 of 6  - Pull down and expand the data/images tree"
@@ -87,8 +94,6 @@ tree -p /data && STEP4="OK - /data/images directory Verified"
 
 read -p "Press the ENTER key to proceed"
 
-## Get and place the data/server2 contents
-
 echo "#############################################################################"
 echo " Step 5 of 6  - Get and place the data/server2 tree"
 echo "#############################################################################"
@@ -105,15 +110,13 @@ tree -p /data && STEP5="OK - /data/server2 directory Verified"
 
 read -p "Press the ENTER key to proceed"
 
-## Do final cleanup of setup steps
-
 echo "#############################################################################"
 echo " Step 6 of 6  - Pull clean default.conf and restart NGINX."
 echo "#############################################################################"
 sudo wget -qP /etc/nginx/conf.d https://raw.githubusercontent.com/learnf5/nginx/main/lab02/default.conf && ls -l /etc/nginx/conf.d/ && sudo nginx -s reload && STEP6="OK - Clean Default.conf and NGINX Reloaded"
 
 echo "#############################################################################"
-echo "################################# RESULTS ###################################"
+echo "################################# \e[0;32mRESULTS\e[0m ###################################"
 echo -e "# If all Steps show a \e[0;32mOK - XXXX Verified\e[0m message the System is setup for Lab Usage"
 echo "#############################################################################"
 echo "#############################################################################"
